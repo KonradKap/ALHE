7e82461680f45111664e5fea43efee66318bf0e8
Marcin <marcinnsuwala@gmail.com>
2017-06-11 11:59:56 +0200

    Out done.

---
+++ out.py
@@ -0,0 +1,62 @@
+import csv
+FILE_WITH_DIET = 'resources/diet'
+FORMAT = ['CALORIES', 'PROTEINS', 'CARBS', 'FATS']
+
+def out(food_dict, days, eaten_nutrients, goal, target_function, count):
+	filename = '{}{}.csv'.format(FILE_WITH_DIET, count)
+	with open(filename, 'w', newline='') as csvfile:
+		spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		spamwriter.writerow(['FUNKCJA CELU', target_function])
+		spamwriter.writerow(['CEL'])
+		spamwriter.writerow(FORMAT)
+		spamwriter.writerow(goal)
+		spamwriter.writerow(['SPOZYTE'])
+		spamwriter.writerow(FORMAT)
+		spamwriter.writerow(eaten_nutrients)
+
+		calories = []
+
+		proteins = []
+		carbs = []
+		fats = []
+		nutrients = [ calories, proteins, carbs, fats]
+		for name, table in zip(FORMAT, nutrients):
+			table.append(name) 
+
+
+		column_names = []
+		meals = []
+		count = 0
+		column_names.append('POSILEK')
+
+		for day in days:
+			column_names.append('DZIEN {0}'.format(count + 1))
+			result = _get_eaten_in_day(day, food_dict)
+			for i, component in zip(range(4), nutrients):
+				component.append(result[i])
+
+			count += 1
+		count = 0
+		print(nutrients)	
+		for meal in food_dict:
+			meal_quantity = []
+			meal_quantity.append(meal)
+			for day in days:
+				meal_quantity.append(day[count])
+			meals.append(meal_quantity)
+
+			count += 1
+
+		spamwriter.writerow(column_names)
+		for meal_quantity in meals:
+			spamwriter.writerow(meal_quantity)
+		for component in nutrients:
+			spamwriter.writerow(component)
+
+
+def _get_eaten_in_day(day, food_dict):
+	result = [0]*4
+	for amount, food in zip(day, food_dict.values()):
+		for i in range(4):
+			result[i] += amount*food[i]
+	return result
